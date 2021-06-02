613297d12815d44bf543a0c8a29184800f660826
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-02 17:49:16 +0100

    add nested select  and answers

--- answers.sql
+++ answers.sql
@@ -263,3 +263,53 @@ Medicine	2
 Peace	1
 Physics	1
 
+--nested select
+-- 1
+SELECT name FROM world
+  WHERE population >
+      (SELECT population FROM world
+        WHERE name='Russia');
+-- 2
+SELECT name FROM world
+  WHERE continent = 'Europe' 
+    AND gdp/population > 
+      (SELECT gdp/population FROM world
+        WHERE name = 'United Kingdom')
+-- 3
+SELECT name, continent FROM world
+  WHERE continent IN
+    (SELECT DISTINCT continent FROM world
+      WHERE name IN ('Argentina', 'Australia'))
+        ORDER BY name;
+-- 4
+SELECT name, population FROM world
+  WHERE population > (SELECT population FROM world WHERE name = 'Canada')
+    AND population < (SELECT population FROM world WHERE name = 'Poland');
+-- 5
+SELECT name, CONCAT(ROUND(100*population/(SELECT population FROM world WHERE name = 'Germany'), 0), '%') AS percentage FROM world WHERE continent = 'Europe';
+-- 6
+SELECT name FROM world
+  WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp IS NOT NULL)
+-- 7
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent = x.continent
+          AND area IS NOT NULL);
+-- 8
+SELECT DISTINCT continent, name FROM world x
+  WHERE name <= ALL (SELECT name FROM world y
+                      WHERE x.continent = y.continent);
+-- 9 
+SELECT name, continent, population FROM world
+  WHERE continent IN
+    (SELECT DISTINCT continent FROM world x
+      WHERE 25000000 >=
+        ALL(SELECT population FROM world y
+          WHERE x.continent = y.continent))
+-- 10
+SELECT name, continent FROM world x 
+  WHERE x.population/3 >=
+    ALL(SELECT population FROM world y
+      WHERE y.continent = x.continent
+        AND x.name != y.name) 
