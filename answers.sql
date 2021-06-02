8c1f7c4312c179ead4094075214b3300abbd7224
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-02 16:57:42 +0100

    add select from noble and answers

--- answers.sql
+++ answers.sql
@@ -151,3 +151,67 @@ SELECT name
 
 Brazil
 Colombia
+
+--SELECT FROM nobel
+--1
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+ --2
+ SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+--3
+SELECT yr, subject
+FROM nobel
+WHERE winner = 'Albert Einstein'
+--4
+SELECT winner
+FROM nobel
+WHERE subject = 'Peace' AND yr >= 2000
+--5
+SELECT yr, subject, winner
+FROM nobel
+WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989
+--6
+SELECT * FROM nobel
+ WHERE winner IN('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
+--7
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John%'
+--8
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject = 'Physics' OR yr = 1984 AND subject = 'Chemistry' 
+--9
+SELECT *
+FROM nobel
+WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine')
+--10
+SELECT *
+FROM nobel
+WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= 2004
+--11
+SELECT *
+FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
+--12
+--Find all details of the prize won by EUGENE O'NEILL
+
+--Escaping single quotes
+--Answer
+SELECT *
+FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
+--13
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner
+--14
+SELECT winner, subject, subject IN ('Physics','Chemistry')
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics','Chemistry'),subject,winner
