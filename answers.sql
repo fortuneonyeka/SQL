25d09f8c46347384722e9f1ad42bc0fdb813dd77
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-02 17:15:27 +0100

    add select from noble Quiz and answers

--- answers.sql
+++ answers.sql
@@ -215,3 +215,51 @@ SELECT winner, subject, subject IN ('Physics','Chemistry')
 FROM nobel
 WHERE yr=1984
 ORDER BY subject IN ('Physics','Chemistry'),subject,winner
+
+--nobel Quiz
+--1 Pick the code which shows the name of winner's names beginning with C and ending in n
+--Answer
+SELECT winner FROM nobel
+ WHERE winner LIKE 'C%' AND winner LIKE '%n'
+--2 Select the code that shows how many Chemistry awards were given between 1950 and 1960
+--Answer
+SELECT COUNT(subject) FROM nobel
+ WHERE subject = 'Chemistry'
+   AND yr BETWEEN 1950 and 1960
+--3 Pick the code that shows the amount of years where no Medicine awards were given
+--Answer
+SELECT COUNT(DISTINCT yr) FROM nobel
+ WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine')
+--4 
+SELECT subject, winner FROM nobel WHERE winner LIKE 'Sir%' AND yr LIKE '196%'
+--Answer
+
+Medicine	Sir John Eccles
+Medicine	Sir Frank Macfarlane Burnet
+--5 Select the code which would show the year when neither a Physics or Chemistry award was given
+--Answer
+SELECT yr FROM nobel
+ WHERE yr NOT IN(SELECT yr 
+                   FROM nobel
+                 WHERE subject IN ('Chemistry','Physics'))
+--6 Select the code which shows the years when a Medicine award was given but no Peace or Literature award was
+--Answer
+SELECT DISTINCT yr
+  FROM nobel
+ WHERE subject='Medicine' 
+   AND yr NOT IN(SELECT yr FROM nobel 
+                  WHERE subject='Literature')
+   AND yr NOT IN (SELECT yr FROM nobel
+                   WHERE subject='Peace')
+--7
+ SELECT subject, COUNT(subject) 
+   FROM nobel 
+  WHERE yr ='1960' 
+  GROUP BY subject
+--Answer
+Chemistry	1
+Literature	1
+Medicine	2
+Peace	1
+Physics	1
+
