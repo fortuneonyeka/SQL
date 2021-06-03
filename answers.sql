95724d1621950e14f188411bff64b02a1819819c
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-03 15:40:45 +0100

    add NULL

--- answers.sql
+++ answers.sql
@@ -537,3 +537,36 @@ A Bronx Tale	1993
 Bang the Drum Slowly	1973
 Limitless	2011
 
+-- Nulls
+-- 1
+SELECT name FROM teacher WHERE dept IS NULL
+-- 2
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+-- 3
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+-- 4
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+-- 5
+SELECT name, COALESCE(mobile, '07986 444 2266') FROM teacher
+-- 6
+SELECT teacher.name, COALESCE(dept.name, 'None') FROM teacher LEFT JOIN dept ON teacher.dept = dept.id
+-- 7
+SELECT COUNT(name), COUNT(mobile) FROM teacher
+-- 8
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON teacher.dept = dept.id GROUP BY dept.name
+-- 9
+SELECT name, CASE WHEN dept IN (1, 2) THEN 'Sci' ELSE 'Art' END AS name FROM teacher
+-- 10
+SELECT name, 
+        CASE 
+          WHEN dept IN (1, 2) THEN 'Sci'
+          WHEN dept = 3 THEN 'Art'
+        ELSE 'None'
+        END AS name 
+          FROM teacher
