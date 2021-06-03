a895ea8aa4087c1c13e1406dc81cac2cbd67fe75
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-03 15:41:16 +0100

    add NULL Quiz

--- answers.sql
+++ answers.sql
@@ -570,3 +570,24 @@ SELECT name,
         ELSE 'None'
         END AS name 
           FROM teacher
+
+-- NULL Quiz 6
+-- 1
+SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
+-- 2
+SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
+-- 3
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
+-- 4
+display 0 in result column for all teachers without department
+-- 5
+'four' for Throd
+-- 6
+-- Table-A
+Shrivell	Computing
+Throd	Computing
+Splint	Computing
+Spiregrain	Other
+Cutflower	Other
+Deadyawn	Other
+
